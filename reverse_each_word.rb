86c31bc80c9f75d4f91c02bf98d0f6f8b722ea48
Jeremy Hill <j.c.hillmail@gmail.com>
2019-07-23 23:28:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(string)
-  string.reverse
   array = string.split
-  array.join
-  
+  new_array = []
+  array.each do |word|
+    new_array << word.reverse
+  end
+    new_array.join(", ")
 end 
